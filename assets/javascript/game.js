d473d8da4b1c79ff9fd81116e40713f6c5859b26
Khamhuang, David <khameye19888@gmail.com>
2021-08-31 21:39:29 -0500

    full list made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,3 @@
-var legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", ]
+// these are the words used for the game
+var legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson"];
+
