d848ccc5295719630eb692232c6892443be5abb8
Khamhuang, David <khameye19888@gmail.com>
2021-09-22 11:26:57 -0500

    updated const

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,3 @@
 // these are the words used for the game
-var legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson"];
+const legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson"];
 
