89b991451c2422169ce9498d97790afa73b97d47
Khamhuang, David <khameye19888@gmail.com>
2021-10-05 11:18:22 -0500

    added random generator

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,7 @@
 // these are the words used for the game
 const legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson"];
 
+let randomLegend = legends[Math.floor(Math.random() * legends.length)];
+// this should randomize the object of the game
+console.log(randomLegend); 
+
