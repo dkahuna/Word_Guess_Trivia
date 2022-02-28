300ba487f970e63bc52d6307fd917f6fb08c8e67
Khamhuang, David <khameye19888@gmail.com>
2022-02-28 10:32:09 -0600

    functionality remake

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // these are the words used for the game
-const legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson"];
+const legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson", "Mad Maggie"];
 
 let randomLegend = legends[Math.floor(Math.random() * legends.length)];
 // this should randomize the object of the game
