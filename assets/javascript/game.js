34fd70fa08a52cfbd20a97b5f772156fa0b036bd
Khamhuang, David <khameye19888@gmail.com>
2022-03-03 02:20:50 -0600

    added more functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,21 @@ let randomLegend = "";
 // Storing the letters of the chosen character into an array
 let letterBlanks = [];
 
+// The amount of blanks for the random character chosen
+let numBlanks = 0;
+
+// Will carry the correct letters guessed and blanks for the game
+let blanksAndSuccesses = [];
+
+// letters guessed
+var letters = [];
+
 // game progession
 let winCounter = 0;
 let lossCounter = 0;
 let guessLeft = 7;
 
-// letters guessed
-var letters = [];
-
+// =====================================
 
 // Functions created for the game
 function startGame() {
@@ -25,6 +32,8 @@ function startGame() {
 randomLegend = legends[Math.floor(Math.random() * legends.length)];
 console.log(randomLegend); 
 
+// Splitting the random legend into individual letters to the variable on line 8
+letterBlanks = randomLegend.split(""); 
 }
 
 startGame();
