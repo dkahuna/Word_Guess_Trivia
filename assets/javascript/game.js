9fbf2425ac41756b520a42c77e2c91f157647ac0
Khamhuang, David <khameye19888@gmail.com>
2022-05-30 14:13:47 -0500

    printing keystrokes with onkeyup event

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,13 @@
 // these are the characters used for the game
-const legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson", "Mad Maggie", "New Castle"];
+let legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson", "Mad Maggie", "New Castle"];
 
 // storing the random Legend for the game
 let randomLegend = "";
+
+// game progession
+let winCounter = 0;
+let lossCounter = 0;
+let guessLeft = 7;
 
 // Storing the letters of the chosen character into an array
 let letterBlanks = [];
@@ -16,29 +21,20 @@ let blanksAndSuccesses = [];
 // letters guessed
 var letters = [];
 
-// game progession
-let winCounter = 0;
-let lossCounter = 0;
-let guessLeft = 7;
 
 // =====================================
 
-// Functions created for the game
-function startGame() {
-    // For start and/or resetting of the game
-    let guessLeft = 7;
-
-    // this should randomize the object of the game
+// randomly chooses from the array to make blanks/object of game
 randomLegend = legends[Math.floor(Math.random() * legends.length)];
+
+// Testing for random character
+console.log(randomLegend); 
 
 // Splitting the random legend into individual letters to the variable
 letterBlanks = randomLegend.split(""); 
 
 // Assigning the amount of blanks from the random legend chosen
 numBlanks = letterBlanks.length;
-
-// Testing for random character
-console.log(randomLegend); 
 
 // An array to setup for the blanks from the random characther to show on the page
 legendBlanks = [];
@@ -53,12 +49,38 @@ console.log(legendBlanks);
 // Prints the amount of blanks from the random character chosen
 document.getElementById("blanks").innerHTML = legendBlanks.join(" ");
 
+// Printing the start of guesses for the user, which is 7
+let remainGuesses = function() {
+    document.querySelector("#guess").innerHTML = guessLeft;
+    console.log(guessLeft);
+}
+// invoking the function to populate the number of guesses
+remainGuesses();
 
-
+// displaying the guesses given and 'joining' a comma to seperate each guess
+let currGuesses = function() {
+    document.querySelector("#usedGuess").innerHTML = letters.join(", ");
 };
 
 
+// ==========================================
+
+// // Initalizing the GAME
+// startGame();
+
+// User presses key(letter guess) function is called....
+document.onkeyup = function(event) {
+    
+    // reducing the amount of guesses per User's attempt to solve game
+    guessLeft--;
+
+    // capturing what the user has guessed
+    let chosenLetters = event.key.toLowerCase();
+
+    // showing what the User has guessed thus far
+    letters.push(chosenLetters);
 
 
-// Initalizing the GAME
-startGame();
+    currGuesses();
+    
+}
