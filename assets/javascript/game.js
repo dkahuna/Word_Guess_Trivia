50bd7675746936eaf49cc3b4e11f757714c74694
Khamhuang, David <khameye19888@gmail.com>
2022-05-30 21:28:52 -0500

    refreshed with vanilla jS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,41 @@
 // these are the characters used for the game
-let legends = ["Bangalore", "Lifeline", "Bloodhound", "Valkyrie", "Caustic", "Wraith", "Pathfinder", "Fuse", "Crypto", "Gibraltar", "Horizon", "Octane", "Mirage", "Loba", "Rampart", "Revenant", "Seer", "Wattson", "Mad Maggie", "New Castle"];
+let legends = ["bangalore", "lifeline", "bloodhound", "valkyrie", "caustic", "wraith", "pathfinder", "fuse", "crypto", "gibraltar", "horizon", "octane", "mirage", "loba", "rampart", "revenant", "seer", "wattson", "mad maggie", "new castle"];
 
-// storing the random Legend for the game
-let randomLegend = "";
+// randomly chooses from the array to make blanks/object of game
+let randomLegend = Math.floor(Math.random()*20);
 
-// game progession
-let winCounter = 0;
-let lossCounter = 0;
-let guessLeft = 7;
+// Storing of the chosen character
+let answer = legends[randomLegend];
 
-// Storing the letters of the chosen character into an array
-let letterBlanks = [];
+// populate the underscore for the game
+let gameBlanks = answer.length;
 
-// The amount of blanks for the random character chosen
-let numBlanks = 0;
+// storing the amount of underscores for the game
+let display = [gameBlanks];
 
-// Will carry the correct letters guessed and blanks for the game
-let blanksAndSuccesses = [];
+// winning the game
+let win = gameBlanks;
 
-// letters guessed
-var letters = [];
+// creating an array for the game from the math random
+let word = answer.split(' ');
 
+// given chances for user to guess
+let attemptsLeft = 7;
+
+let output = " ";
+
+let userLetter = "";
 
 // =============Basis of the Game========================
 
-// randomly chooses from the array to make blanks/object of game
-randomLegend = legends[Math.floor(Math.random() * legends.length)];
-
-// Testing for random character
-console.log(randomLegend); 
-
-// Splitting the random legend into individual letters to the variable
-letterBlanks = randomLegend.split(""); 
-
-// Assigning the amount of blanks from the random legend chosen
-numBlanks = letterBlanks.length;
-
-// An array to setup for the blanks from the random characther to show on the page
-legendBlanks = [];
-
-// Pushing the set of "blanks" for the game
-for (var i = 0; i < numBlanks; i++) {
-    legendBlanks.push("_");
+var setup = function() {
+    for (var i = 0; i < answer.length; i++)
+    {
+        display[i] = "_ ";
+        output = output + display[i];
+    }
+    document.getElementById("blanks").innerHTML = output;
+    output = " ";
 }
 
-console.log(legendBlanks);
-
-// Prints the amount of blanks from the random character chosen
-document.getElementById("blanks").innerHTML = legendBlanks.join(" ");
-
-// Printing the start of guesses for the user, which is 7
-let remainGuesses = function() {
-    document.querySelector("#guess").innerHTML = guessLeft;
-    console.log(guessLeft);
-}
-// invoking the function to populate the number of guesses
-remainGuesses();
-
-// displaying the guesses given and 'joining' a comma to seperate each guess
-let currGuesses = function() {
-    document.querySelector("#usedGuess").innerHTML = letters.join(", ");
-};
-
-// ==============Events============================
-
-// User presses key(letter guess) function is called....
-document.onkeyup = function(event) {
-    
-    // reducing the amount of guesses per User's attempt to solve game
-    guessLeft--;
-    // when the "chances" reach zero, end of game
-    if (guessLeft === 0) {
-        lossCounter++;
-        document.querySelector("#losses").innerHTML = lossCounter;
-    };
-
-    // capturing what the user has guessed
-    let chosenLetters = event.key.toLowerCase();
-
-    // showing what the User has guessed thus far
-    letters.push(chosenLetters);
-
-    // invoking the functions within the event will update the game for the letters guessed and how many chances are left
-    currGuesses();
-    remainGuesses();
-
-
-    // checking if "guess" matches string of game
-};
+setup();
